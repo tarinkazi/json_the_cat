cee0b383f495de6fd98650bc99ffacbf1e35833c
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-04 18:51:35 -0400

    done

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,38 +1,28 @@
 const request = require('request');
-const domain = process.argv[2]
+const domain = process.argv[2];
 
 //const urlName = 'https://api.thecatapi.com/v1/breeds/search?q=sib';//`http://${domain}/`
 const url = 'https://api.thecatapi.com/v1/breeds/search?q=';//`h
-let urlName = url+""+domain;
-
-let data;
+let urlName = url + "" + domain;
 
 
-//if(url){
-  //console.log("valid search");
-//}
-//else {
-  request(urlName, (error, response, body) => {
-    if(error) {
-      console.log('Sorry request url not found'); 
-      process.exit();
-    }
-  
-  
-      let data = JSON.parse(body);
 
-  
-  if(!data[0]){
-    console.log("not exist");
+request(urlName, (error, response, body) => {
+  if (error) {
+    console.log('Sorry request url not found');
     process.exit();
   }
-  else{
-   
+  let data = JSON.parse(body);
+
+  
+  if (!data[0]) {
+    console.log("not exist");
+    process.exit();
+  } else {
     console.log(data[0].description);
   }
+});
 
-   });
-//const filPath = '../page-fetcher/index.html';
 
   
 
